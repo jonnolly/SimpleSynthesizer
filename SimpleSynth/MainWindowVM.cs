9971559232172d12aca6d32c268e06089cd39cdf
unknown <john_connolly1992@hotmail.com>
2020-01-14 19:09:28 +0100

    commit

---
+++ SimpleSynth/MainWindowVM.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Windows.Input;
+
+namespace SimpleSynth
+{
+    public class MainWindowVM : ViewModelBase
+    {
+
+        private const int SAMPLE_RATE = 44100;
+        private const short BITS_PER_SAMPLE = 16;
+
+        public ICommand GenerateSound { get { return new DelegateCommand(_generateSound, null); } }
+
+        private void _generateSound(object commandParameter)
+        {
+            short[] wave = new short[SAMPLE_RATE];
+            byte[] binaryWave = new byte[SAMPLE_RATE * sizeof(short)];
+
+            float frequency = 240f;
+            for (int i = 0; i < SAMPLE_RATE; i++)
+            {
+                wave[i] = Convert.ToInt16(short.MaxValue * Math.Sin(((Math.PI * 2 * frequency) / SAMPLE_RATE) * i ));
+            }
+
+            Buffer.BlockCopy(wave, 0, binaryWave, 0, wave.Length * sizeof(short));
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            using (BinaryWriter binaryWriter = new BinaryWriter(memoryStream))
+            {
+                short blockAlign = BITS_PER_SAMPLE / 8;
+                int subChunkTwoSize = SAMPLE_RATE * blockAlign;
+                binaryWriter.Write(new[] { 'R', 'I', 'F', 'F' });
+                binaryWriter.Write(36 + subChunkTwoSize);
+                binaryWriter.Write(new[] { 'W', 'A', 'V', 'E', 'f', 'm', 't', ' ' });
+                binaryWriter.Write(16);
+                binaryWriter.Write((short)1);
+                binaryWriter.Write((short)1);
+                binaryWriter.Write(SAMPLE_RATE);
+                binaryWriter.Write(SAMPLE_RATE * blockAlign);
+                binaryWriter.Write(blockAlign);
+                binaryWriter.Write(BITS_PER_SAMPLE);
+                binaryWriter.Write(new[] { 'd', 'a', 't', 'a' });
+                binaryWriter.Write(subChunkTwoSize);
+                binaryWriter.Write(binaryWave);
+
+                memoryStream.Position = 0;
+
+                new SoundPlayer(memoryStream).Play();
+            }
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
